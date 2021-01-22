691f46afd4fb9efe45fb98db9d7081a1afc5f2e0
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-01-22 17:08:18 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-PUNCTUATION = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'
-
+import re 
 
 def top_3_words(text):
-    dict_of_words = dict()
+    rgx = r'\'{0,1}[a-zA-Z]+\'*[a-zA-Z]*'
+    words = re.findall(rgx, text)
+    frq = {}
+    for word in words:
+        frq.setdefault(word.lower(), 0)
+        frq[word.lower()] += 1
 
-    for index, value in enumerate(text):
-        if value in PUNCTUATION:
-            text = text.replace(value, ' ')
-
-    list_of_words = text.split()
-
-    for i in list_of_words:
-        pu = [j for j in i for i in list_of_words if (j == "'") or (j == '"')]
-        if len(pu) == len(i):
-            while i in list_of_words:
-                list_of_words.remove(i)
-
-    print(list_of_words)
-    for i in list_of_words:
-        dict_of_words[i.lower()] = text.split().count(i)
-
-    result = sorted(dict_of_words, key=dict_of_words.get, reverse=True)[:3]
-    return result
-
-
-print(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e ''"))
+    result = sorted(frq, key=lambda x: frq[x], reverse=True)
+    
+    return result[:3]
